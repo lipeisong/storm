be15f9f32d82e21c8f228af338ffd23a3be5b4c5
lipeisong <peso_2016@126.com>
2018-09-21 20:14:17 +0800

    初始化redis分支

--- src/main/java/com/imooc/bigdata/Integration/demo/HelloWorld.java
+++ src/main/java/com/imooc/bigdata/Integration/demo/HelloWorld.java
@@ -2,8 +2,6 @@ package com.imooc.bigdata.Integration.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        for(int i=0;i<10;i++){
-            System.out.println(i);
-        }
+
     }
 }
